01591c19ef47ea4295feba7e08ed3533c9f46c48
Joyce <josabavi123@gmail.com>
2023-10-19 23:58:53 -0500

    first version of relationships of the bounded context profile

--- PERUSTARS/PERUSTARS/Shared/Infrastructure/Configuration/AppDbContext.cs
+++ PERUSTARS/PERUSTARS/Shared/Infrastructure/Configuration/AppDbContext.cs
@@ -6,6 +6,9 @@ namespace PERUSTARS.Shared.Infrastructure.Configuration
     public class AppDbContext: DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Hobbyist> Hobbyists { get; set; }
+        public DbSet<Follower> Followers { get; set; }
 
         public AppDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
         {
@@ -20,6 +23,27 @@ namespace PERUSTARS.Shared.Infrastructure.Configuration
             builder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
             // ...Poner más atributos ...
 
+
+            builder.Entity<Artist>().ToTable("artists");
+            
+           // builder.Entity<Artist>().HasOne<Artist>(a => a.User)
+                    //.WithOne(u => u.Artist)
+                    //.HasForeignKey<Artist>(a => a.Id);
+
+                    builder.Entity<Hobbyist>().ToTable("hobbyists");
+                //.HasOne(u => u.User)
+               // .WithOne(h => h.Hobbyst)
+               // .HasForeignKey<Hobbyist>(h => h.Id);
+
+            //builder.Entity<Hobbyist>().HasOne(h => h.Follower)
+              //  .WithOne(f => f.Hobbyist)
+              //  .HasForeignKey<Follower>(f => f.HobbyistId);
+
+            //builder.Entity<Artist>().HasMany(a => a.Follower)
+             //   .WithOne(f => f.Artist)
+             //   .HasForeignKey(f => f.ArtistId);
+
+            builder.Entity<Follower>().ToTable("followers");
 
         }
     }
