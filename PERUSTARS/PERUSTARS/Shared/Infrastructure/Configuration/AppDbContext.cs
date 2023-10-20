3cf4af5691f4a6a0082fa59ab515a6d127f7ddd4
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-10-19 22:37:30 -0500

    feat: added identity and account management relational mapping

--- PERUSTARS/PERUSTARS/Shared/Infrastructure/Configuration/AppDbContext.cs
+++ PERUSTARS/PERUSTARS/Shared/Infrastructure/Configuration/AppDbContext.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PERUSTARS.Domain.Models;
 
 namespace PERUSTARS.Shared.Infrastructure.Configuration
 {
     public class AppDbContext: DbContext
     {
+        public DbSet<User> Users { get; set; }
+
         public AppDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
         {
 
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(builder);
+            builder.Entity<User>().ToTable("users");
+            builder.Entity<User>().HasKey(u => u.Id);
+            builder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
+            // ...Poner más atributos ...
+
 
         }
     }
