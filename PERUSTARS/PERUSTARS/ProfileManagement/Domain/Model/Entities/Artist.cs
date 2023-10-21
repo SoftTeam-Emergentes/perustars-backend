9b8993a8711aeec607d0664b37f94d5868ddc9e7
Joyce <josabavi123@gmail.com>
2023-10-20 19:00:08 -0500

    Add model and enum

--- PERUSTARS/PERUSTARS/ProfileManagement/Domain/Model/Entities/Artist.cs
+++ PERUSTARS/PERUSTARS/ProfileManagement/Domain/Model/Entities/Artist.cs
@@ -1,21 +1,27 @@
 using System.Collections.Generic;
 using System.Numerics;
 using PERUSTARS.Domain.Models;
+using PERUSTARS.ProfileManagement.Domain.Model.Enum;
+using User = PERUSTARS.IdentityAndAccountManagement.Domain.Model.User;
 
 
 namespace PERUSTARS.ProfileManagement.Domain.Model.Entities
 {
-    public class Artist
+    public class Artist : User
     {
         public BigInteger ArtistId { get; set; }
-        public string BrandName { get; set; }
+        public string BrandName { get; set; } //Nickname
         public string Description { get; set; }
         public string Phrase { get; set; }
+        public int ContactNumber { get; set; }
+        public string ContactEmail { get; set; }
+        public int Age { get; set; }
+        public Genre Genre { get; set; }
+        
+        public List<string>SocialMediaLink { get; set; } //SocialNetwork
 
-        public List<string>SocialMediaLink { get; set; }
-
-        public long SpecialtyId { get; set; }
-        public Specialty SpecialtyArt { get; set; }
+        //public long SpecialtyId { get; set; }
+        //public Specialty SpecialtyArt { get; set; }
 
         public IList<Artwork> Artworks { get; set; } = new List<Artwork>();
 
--- PERUSTARS/PERUSTARS/ProfileManagement/Domain/Model/Entities/Hobbyist.cs
+++ PERUSTARS/PERUSTARS/ProfileManagement/Domain/Model/Entities/Hobbyist.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using System.Numerics;
 using PERUSTARS.Domain.Models;
+using User = PERUSTARS.IdentityAndAccountManagement.Domain.Model.User;
 
 namespace PERUSTARS.ProfileManagement.Domain.Model.Entities
 {
-    public class Hobbyist
+    public class Hobbyist: User
     {
         public BigInteger HobbyisttId { get; set; }
+        public int Age { get; set; }
         public List<Interest> Interests { get; set; }
         public List<FavoriteArtwork> FavoriteArtworks { get; set; }
 
