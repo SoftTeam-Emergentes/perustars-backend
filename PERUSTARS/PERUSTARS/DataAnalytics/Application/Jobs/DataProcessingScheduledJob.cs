a6b86d9aae607c8e3cf768a618aec0d48ed01dc9
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-10-24 00:50:20 -0500

    feat: added data processing scheduled job

--- PERUSTARS/PERUSTARS/DataAnalytics/Application/Jobs/DataProcessingScheduledJob.cs
+++ PERUSTARS/PERUSTARS/DataAnalytics/Application/Jobs/DataProcessingScheduledJob.cs
@@ -1,6 +1,37 @@
-﻿namespace PERUSTARS.DataAnalytics.Application.Jobs
+﻿using Microsoft.Extensions.Hosting;
+using PERUSTARS.DataAnalytics.Domain.Services;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PERUSTARS.DataAnalytics.Application.Jobs
 {
-    public class DataProcessingScheduledJob
+    public class DataProcessingScheduledJob : IHostedService, IDisposable
     {
+        private Timer _timer;
+        private readonly IDataAnalyticsCommandService _dataAnalyticsCommandService;
+
+        public DataProcessingScheduledJob(IDataAnalyticsCommandService dataAnalyticsCommandService)
+        {
+            _dataAnalyticsCommandService = dataAnalyticsCommandService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(ProcessDataAndSendToML, null, TimeSpan.Zero, TimeSpan.FromHours(5));
+            return Task.CompletedTask;
+        }
+
+        public void ProcessDataAndSendToML(object state)
+        {
+           
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+        public void Dispose() => _timer?.Dispose();
     }
 }
