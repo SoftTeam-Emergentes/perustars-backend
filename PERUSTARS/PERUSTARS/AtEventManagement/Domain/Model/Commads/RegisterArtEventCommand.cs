0a259c744487aca1d9f18a19f79b6f2d27c476de
FranchescoSoto <u201821078@upc.edu.pe>
2023-10-25 16:22:30 -0500

    feat(artevent): add commands

---
+++ PERUSTARS/PERUSTARS/AtEventManagement/Domain/Model/Commads/RegisterArtEventCommand.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace PERUSTARS.AtEventManagement.Domain.Model.Commads
+{
+    public record RegisterArtEventCommand(string title,string description,DateTime StartDatetime,);
+
+}
