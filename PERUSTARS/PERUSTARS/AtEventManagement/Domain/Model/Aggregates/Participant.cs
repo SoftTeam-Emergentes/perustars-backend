c291897d19b38f4e7cd4b08b3d58875891b6ad48
FranchescoSoto <u201821078@upc.edu.pe>
2023-10-25 23:15:02 -0500

    feat: fix properties

--- PERUSTARS/PERUSTARS/AtEventManagement/Domain/Model/Aggregates/Participant.cs
+++ PERUSTARS/PERUSTARS/AtEventManagement/Domain/Model/Aggregates/Participant.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Numerics;
 using PERUSTARS.ProfileManagement.Domain.Model.Aggregates;
-using PERUSTARS.ProfileManagement.Domain.Model.Entities;
+
 
 namespace PERUSTARS.AtEventManagement.Domain.Model.Aggregates
 {
@@ -12,14 +11,14 @@ namespace PERUSTARS.AtEventManagement.Domain.Model.Aggregates
         public DateTime RegisterDateTime { get; set; }
         public DateTime CheckInDateTime { get; set; }
         public DateTime ParticipantRegistrationDateTime { get; set; }
-        public long? HobystId { get; set; }
+        public long? HobbyistId { get; set; }
         public Hobbyist Hobyst { get; set; }
         public ArtEvent ArtEvent { get; set; }
         public long? ArtEventId { get; set; }
         public bool Collected { get; set; }
 
 
-        public Participant(long id, string userName, DateTime registerDateTime, DateTime checkInDateTime, long? hobystId, long? artEventId, Hobbyist hobyst, ArtEvent artEvent, bool collected)
+        public Participant(long id, string userName, DateTime registerDateTime, DateTime checkInDateTime, long? hobbyistId, long? artEventId, Hobbyist hobbyist, ArtEvent artEvent, bool collected)
         {
             Id = id;
             UserName = userName;
@@ -27,7 +26,7 @@ namespace PERUSTARS.AtEventManagement.Domain.Model.Aggregates
             CheckInDateTime = checkInDateTime;
             HobbyistId = hobbyistId;
             ArtEventId = artEventId;
-            Hobbyist = hobbyist;
+            Hobyst = hobbyist;
             ArtEvent = artEvent;
             Collected = collected;
         }
