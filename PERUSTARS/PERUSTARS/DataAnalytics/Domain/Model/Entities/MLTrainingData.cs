06842101c6422db8dddf4fa3bfae15333f364499
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-10-26 14:41:55 -0500

    feat: added ML training data model

--- PERUSTARS/PERUSTARS/DataAnalytics/Domain/Model/Entities/MLTrainingData.cs
+++ PERUSTARS/PERUSTARS/DataAnalytics/Domain/Model/Entities/MLTrainingData.cs
@@ -1,8 +1,15 @@
-﻿namespace PERUSTARS.DataAnalytics.Domain.Model.Entities
+﻿using System;
+
+namespace PERUSTARS.DataAnalytics.Domain.Model.Entities
 {
     public class MLTrainingData
     {
-        public long ArtistRecommendationArtistId;
-        public long ArtistRecommendationHobbyistId;
+        public long ArtistRecommendationArtistId { get; set; }
+        public long ArtistRecommendationHobbyistId { get; set; }
+        public DateTime ArtistRecommendationDatetime { get; set; }
+        public long FollowerHobyistId { get; set; }
+        public DateTime FollowerRegistrationDatetime { get; set; }
+        public long EventParticipantHobyistId { get; set; }
+        public DateTime EventParticipantRegistrationDatetime { get; set; }
     }
 }
