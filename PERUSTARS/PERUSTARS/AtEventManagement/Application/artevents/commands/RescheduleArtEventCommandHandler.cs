61e4b24c7404340664b977215a6074f254481e5f
FranchescoSoto <u201821078@upc.edu.pe>
2023-10-27 10:58:31 -0500

    feat: add art event command handlers

---
+++ PERUSTARS/PERUSTARS/AtEventManagement/Application/artevents/commands/RescheduleArtEventCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using PERUSTARS.AtEventManagement.Domain.Model.Aggregates;
+using PERUSTARS.AtEventManagement.Domain.Model.Commads;
+using PERUSTARS.AtEventManagement.Domain.Model.Repositories;
+using PERUSTARS.AtEventManagement.Domain.Model.ValueObjects;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PERUSTARS.AtEventManagement.Application.artevents.commands
+{
+    public class RescheduleArtEventCommandHandler : IRequestHandler<RescheduleArtEventCommand, string>
+    {
+        private readonly IArtEventRepository _artEventRepository;
+        public RescheduleArtEventCommandHandler(IArtEventRepository artEventRepository)
+        {
+            _artEventRepository = artEventRepository;
+        }
+        public async Task<string> Handle(RescheduleArtEventCommand request, CancellationToken cancellationToken)
+        {
+            ArtEvent artEvent = await _artEventRepository.FindByIdAsync(request.id);
+            if (artEvent != null)
+            {
+                artEvent.StartDateTime = request.newDate;
+                artEvent.CurrentStatus = ArtEventStatus.RESCHEDULED;
+                _artEventRepository.Update(artEvent);
+                return await Task.FromResult("Art Event rescheduled!!");
+            }
+            else {
+                return await Task.FromResult("The art event with the given id doesn't exist");
+            }
+        }
+
+    }
+}
---
+++ PERUSTARS/PERUSTARS/AtEventManagement/Application/artevents/commands/StartArtEventCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using PERUSTARS.AtEventManagement.Domain.Model.Aggregates;
+using PERUSTARS.AtEventManagement.Domain.Model.Commads;
+using PERUSTARS.AtEventManagement.Domain.Model.Repositories;
+using PERUSTARS.AtEventManagement.Domain.Model.ValueObjects;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PERUSTARS.AtEventManagement.Application.artevents.commands
+{
+    public class StartArtEventCommandHandler : IRequestHandler<StartArtEventCommand, string>
+    {
+        private readonly IArtEventRepository _artEventRepository;
+        public StartArtEventCommandHandler(IArtEventRepository artEventRepository)
+        {
+            _artEventRepository = artEventRepository;
+        }
+
+        public async Task<string> Handle(StartArtEventCommand request, CancellationToken cancellationToken)
+        {
+            ArtEvent artEvent = await _artEventRepository.FindByIdAsync(request.id);
+            if (artEvent == null)
+            {
+                return await Task.FromResult("Something went wrong");
+            }
+            else
+            {
+                artEvent.CurrentStatus = ArtEventStatus.STARTED;
+                return await Task.FromResult("Event started!!");
+
+            }
+        }
+    }
+}
