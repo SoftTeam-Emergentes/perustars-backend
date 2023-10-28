4c080287b18d54fcd076a93f6e97483651f50427
FranchescoSoto <u201821078@upc.edu.pe>
2023-10-28 15:51:53 -0500

    add

--- PERUSTARS/PERUSTARS/AtEventManagement/Domain/Services/Participant/IParticipantQueryService.cs
+++ PERUSTARS/PERUSTARS/AtEventManagement/Domain/Services/Participant/IParticipantQueryService.cs
@@ -1,6 +1,13 @@
-﻿namespace PERUSTARS.AtEventManagement.Domain.Services.Participant
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PERUSTARS.AtEventManagement.Domain.Services.Participant
 {
     public interface IParticipantQueryService
     {
+        public PERUSTARS.AtEventManagement.Domain.Model.Aggregates.Participant getParticipantById(int id);
+        public IEnumerable<PERUSTARS.AtEventManagement.Domain.Model.Aggregates.Participant>  getParticipantByHobbyistId(int id);
+        public void deleteParticipantById(int id);
+        public IEnumerable<PERUSTARS.AtEventManagement.Domain.Model.Aggregates.Participant> getParticipantByEventId(int id);
     }
 }
