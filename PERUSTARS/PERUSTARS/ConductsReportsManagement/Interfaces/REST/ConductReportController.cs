cb062b4d7e516063a8703c5951eec41c850bfd72
Alvaro <93229518+Alvaro2003j@users.noreply.github.com>
2023-10-30 17:12:49 -0500

    input Controller

--- PERUSTARS/PERUSTARS/ConductsReportsManagement/Interfaces/REST/ConductReportController.cs
+++ PERUSTARS/PERUSTARS/ConductsReportsManagement/Interfaces/REST/ConductReportController.cs
@@ -1,11 +1,49 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PERUSTARS.ConductsReportsManagement.Domain.Model.Commands;
+using PERUSTARS.ConductsReportsManagement.Domain.Model.Services;
+using PERUSTARS.ConductsReportsManagement.Interfaces.REST.resources;
+using PERUSTARS.ConductsReportsManagement.Interfaces.REST.Resources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PERUSTARS.ConductsReportsManagement.Interfaces.REST
 {
-    public class ConductReportController
+    [Route("api/conductReports/[controller]")]
+    [ApiController]
+    public class ConductReportController : ControllerBase
     {
+        private readonly IConductReportService _conductReportService;
+        private readonly IMapper _mapper;
+
+        public ConductReportController(IConductReportService conductReportService, IMapper mapper)
+        {
+            _mapper = mapper;
+            _conductReportService = conductReportService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterNewConductReport([FromBody] RegisterConductReport registerConductReport)
+        {
+            RegisterConductReportCommand registerConductReportCommand = _mapper.Map<RegisterConductReportCommand>(registerConductReport);
+            ConductReportResource conductReportResource = await _conductReportService.ExecuteRegisterConductReportCommand(registerConductReportCommand);
+            return Ok(conductReportResource);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteConductReport(long _id)
+        {
+            DeleteConductReportCommand deleteConductReportCommand = new DeleteConductReportCommand()
+            {
+                id = _id
+            };
+
+            await _conductReportService.ExecuteDeleteConductReportCommand(deleteConductReportCommand);
+            var successMessage = "Conduct Report was removed";
+            return Ok(successMessage);
+        }
+
     }
 }
