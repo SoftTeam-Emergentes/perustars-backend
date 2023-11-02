e1abce351e9b82743242bc20de8dee3ef5972f8c
FranchescoSoto <u201821078@upc.edu.pe>
2023-11-02 16:53:55 -0500

    feat: fix register participant

--- PERUSTARS/PERUSTARS/AtEventManagement/Application/artevents/commands/RegisterParticipantToArtEventCommandHandler.cs
+++ PERUSTARS/PERUSTARS/AtEventManagement/Application/artevents/commands/RegisterParticipantToArtEventCommandHandler.cs
@@ -3,45 +3,46 @@ using PERUSTARS.AtEventManagement.Domain.Model.Aggregates;
 using PERUSTARS.AtEventManagement.Domain.Model.Commads;
 using PERUSTARS.AtEventManagement.Domain.Model.Repositories;
 using PERUSTARS.ProfileManagement.Domain.Model.Aggregates;
+using PERUSTARS.ProfileManagement.Domain.Repositories;
 using PERUSTARS.Shared.Domain.Repositories;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace PERUSTARS.AtEventManagement.Application.artevents.commands
 {
-    public class RegisterParticipantToArtEventCommandHandler// : IRequestHandler<RegisterParticipantToArtEventCommand, string>
+    public class RegisterParticipantToArtEventCommandHandler : IRequestHandler<RegisterParticipantToArtEventCommand, string>
     {
-        //private readonly IParticipantRepository _participantRepository;
-        //private readonly IHobbyistRepository _hobbyistRepository;
-        //private readonly IArtEventRepository _artEventRepository;
-        //private readonly IUnitOfWork _unitOfWork;
-        //public RegisterParticipantToArtEventCommandHandler(IParticipantRepository participantRepository, IHobbyistRepository hobbyistRepository,IArtEventRepository artEventRepository, IUnitOfWork unitOfWork)
-        //{
-        //    _participantRepository = participantRepository;
-        //    _hobbyistRepository = hobbyistRepository;
-        //    _artEventRepository= artEventRepository;
-        //    _unitOfWork = unitOfWork;
-        //    
-        //}
-        public  string Handle(RegisterParticipantToArtEventCommand request, CancellationToken cancellationToken)
+        private readonly IParticipantRepository _participantRepository;
+        private readonly IHobbyistRepository _hobbyistRepository;
+        private readonly IArtEventRepository _artEventRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public RegisterParticipantToArtEventCommandHandler(IParticipantRepository participantRepository, IHobbyistRepository hobbyistRepository,IArtEventRepository artEventRepository, IUnitOfWork unitOfWork)
         {
-            //ArtEvent artEvent = _artEventRepository.FindByIdAsync(request.artEventId);
-            //Hobbyist hobbyist = _hobbyistRepository.findByIdAsync(request.hobbyistId);
-            //Participant participant = new Participant(
-            //    id: 0,
-            //    userName: hobbyist.FirstName,
-            //    registerDateTime: new System.DateTime(),
-            //    checkInDateTime: null,
-            //    hobbyistId: request.hobbyistId,
-            //    artEventId: request.artEventId,
-            //    hobbyist: hobbyist,
-            //    artEvent: artEvent,
-            //    collected: false);
-            //
-            //
-            //
-            //await _participantRepository.AddAsync(participant);
-            //await _unitOfWork.CompleteAsync();
+            _participantRepository = participantRepository;
+            _hobbyistRepository = hobbyistRepository;
+            _artEventRepository= artEventRepository;
+            _unitOfWork = unitOfWork;
+            
+        }
+        public async  Task<string> Handle(RegisterParticipantToArtEventCommand request, CancellationToken cancellationToken)
+        {
+            ArtEvent artEvent = _artEventRepository.FindByIdAsync(request.artEventId).Result;
+            Hobbyist hobbyist = _hobbyistRepository.FindByIdAsync(request.hobbyistId).Result;
+            PERUSTARS.AtEventManagement.Domain.Model.Aggregates.Participant participant = new PERUSTARS.AtEventManagement.Domain.Model.Aggregates.Participant(
+                id: 0,
+                userName: "Funcion en desarrollo",
+                registerDateTime: new System.DateTime(),
+                checkInDateTime: null,
+                hobbyistId: request.hobbyistId,
+                artEventId: request.artEventId,
+                hobbyist: hobbyist,
+                artEvent: artEvent,
+                collected: false);
+            
+            
+            
+            await _participantRepository.AddAsync(participant);
+            await _unitOfWork.CompleteAsync();
             return "Your participation was registered: ";
         }
     }
