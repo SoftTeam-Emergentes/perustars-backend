462c4f9a3f86169315a60fe257f91c3b9ce10f92
FranchescoSoto <u201821078@upc.edu.pe>
2023-11-02 16:30:11 -0500

    feat: add base test

---
+++ PERUSTARS/PERUSTARS.Test/ArtEventServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PERUSTARS.Test
+{
+    internal class ArtEventServiceTest
+    {
+        [SetUp]
+        public void SetUp() { 
+        }
+        [Test]
+        public async Task GetById() {
+            var mockEventRepository = GetDefaulIArtEventRepository();
+            var mockEvent = mockEventRepository.Get(1);
+                
+        }
+    }
+}
