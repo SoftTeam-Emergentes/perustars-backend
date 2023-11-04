0ac63c69fdd1b883c533e06c99e119332682f829
Joyce <josabavi123@gmail.com>
2023-11-03 22:56:46 -0500

    artist.feature

---
+++ PERUSTARS/PERUSTARS.Test/ProfileManagement/UnitTests/ProfileManagementUnitTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using PERUSTARS.ProfileManagement.Domain.Repositories;
+using PERUSTARS.Test.Infrastructure.Configuration;
+
+namespace PERUSTARS.Test.ProfileManagement.UnitTests
+{
+    [TestFixture]
+    public class ProfileManagementUnitTest
+    {
+        private MockDbContext _context;
+        private IArtistRepository _artistRepository;
+        
+    }
+}
