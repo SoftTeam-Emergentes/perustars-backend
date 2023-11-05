9e0628e4652c06ba861a0c2e9bc53819899dbb0f
Manuel Aranguri <67668320+Nwyr-34@users.noreply.github.com>
2023-11-04 20:31:01 -0500

    feat: 1 test added

---
+++ PERUSTARS/PERUSTARS.Test/NotificationTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MediatR;
+using PERUSTARS.ArtworkManagement.Domain.Repositories;
+using PERUSTARS.CommunicationAndNotificationManagement.Domain.Repositories;
+
+namespace PERUSTARS.Test
+{
+    public class NotificationTest
+    {
+        private MockDbContext _context;
+        private INotificationRepository  _notificationRepository;
+        private IServiceProvider serviceProvider;
+        private IMediator _mediator;
+        private IArtworkRepository _artworkRepository;
+    }
+
+    internal class MockDbContext
+    {
+    }
+}
