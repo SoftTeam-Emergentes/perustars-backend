4ff6548a2fe837a1787dde64e880b3841c0835c4
Joyce <josabavi123@gmail.com>
2023-11-13 17:29:40 -0500

    update in enum attributes

--- PERUSTARS/PERUSTARS/ProfileManagement/Domain/Model/Enum/Genre.cs
+++ PERUSTARS/PERUSTARS/ProfileManagement/Domain/Model/Enum/Genre.cs
@@ -3,9 +3,9 @@ namespace PERUSTARS.ProfileManagement.Domain.Model.Enum
     public enum Genre
     {
         MUSICIAN,
-        Filmmaker,
-        VisualArtist,
-        Writer,
-        Other
+        FILMAKER,
+        VISUALARTIST,
+        WRITER,
+        OTHER
     }
 }
