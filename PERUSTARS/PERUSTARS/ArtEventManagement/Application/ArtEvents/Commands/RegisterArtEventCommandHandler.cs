107d1f6c57c02ca7de5547b5ead0ce885881464c
FranchescoSoto <u201821078@upc.edu.pe>
2023-11-20 23:32:46 -0500

    fix: create art event

--- PERUSTARS/PERUSTARS/ArtEventManagement/Application/ArtEvents/Commands/RegisterArtEventCommandHandler.cs
+++ PERUSTARS/PERUSTARS/ArtEventManagement/Application/ArtEvents/Commands/RegisterArtEventCommandHandler.cs
@@ -5,6 +5,8 @@ using PERUSTARS.ArtEventManagement.Domain.Model.Aggregates;
 using PERUSTARS.ArtEventManagement.Domain.Model.Commads;
 using PERUSTARS.ArtEventManagement.Domain.Model.Repositories;
 using PERUSTARS.ArtEventManagement.Domain.Model.ValueObjects;
+using PERUSTARS.ProfileManagement.Domain.Model.Aggregates;
+using PERUSTARS.ProfileManagement.Domain.Repositories;
 using PERUSTARS.Shared.Domain.Repositories;
 
 namespace PERUSTARS.ArtEventManagement.Application.ArtEvents.Commands
@@ -13,30 +15,32 @@ namespace PERUSTARS.ArtEventManagement.Application.ArtEvents.Commands
     {
         private readonly IArtEventRepository _artEventRepository;
         private readonly IUnitOfWork _unitOfWork;
-        public RegisterArtEventCommandHandler(IArtEventRepository artEventRepository, IUnitOfWork unitOfWork)
+        private readonly IArtistRepository _artistRepository;
+        public RegisterArtEventCommandHandler(IArtEventRepository artEventRepository, IUnitOfWork unitOfWork,IArtistRepository artist)
         {
             _artEventRepository = artEventRepository;
             _unitOfWork = unitOfWork;
+            _artistRepository = artist;
         }
 
         public async Task<string> Handle(RegisterArtEventCommand request, CancellationToken cancellationToken)
         {
             //Primero se debe validar si existe el artista con el Id dado
-
+            Artist artist=await _artistRepository.GetArtistByIdAsync(request.ArtistId);
             //"return No existe el Artista con el Id dado"
             //-----------------------------------
+            if(artist==null) {
+                return "This artist doesn't exist!!";
+            }
+
             var artEvent = new ArtEvent(
-                id: 0,
                 title: request.Title,
                 description: request.Description,
                 startDateTime: request.StartDateTime,
                 Location: request.Location,
                 isOnline: request.IsOnline,
                 artistId: request.ArtistId,
-                currentStatus: ArtEventStatus.REGISTERED,
-                Participants:null,
-                Artist:null,
-                collected:false
+                Artist:artist
                 );
             await _artEventRepository.AddAsync(artEvent);
             await _unitOfWork.CompleteAsync();
